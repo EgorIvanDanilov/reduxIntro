dfde6efe51982585b8ebb18f9dc9f892acc4678a
Egor Danilov <mellowd94@gmail.com>
2023-11-03 19:05:44 +0100

    add BrowserRouter

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -5,10 +5,13 @@ import { store } from "./app/store"
 import App from "./App"
 import "./index.css"
 import Products from "./features/products/Products"
+// import { Browser}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
-    <Products />
+    <BrowserRouter>
+        <App />
+        <Products />
+    </BrowserRouter>
   </Provider>,
 )
